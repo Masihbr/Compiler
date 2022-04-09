e261ce69918e0791fabf2df740a8ee040b48096b
moroukian <moroukianali@gmail.com>
2022-04-10 01:57:11 +0430

    Set up base structure

---
+++ compiler.py
@@ -0,0 +1,11 @@
+"""
+Masih Beigi - 98101244
+Ali Moroukian - 98106094
+"""
+
+from scanner import Scanner
+
+if __name__ == '__main__':
+    scanner = Scanner()
+    while True:
+        scanner.get_next_token()
