b43390ac247c370ce0d35b355252b581e4b805f5
masihbr <masihbr@gmail.com>
2022-04-10 19:25:31 +0430

    add file handler to read and write files

---
+++ file_handler.py
@@ -0,0 +1,18 @@
+class FileHandler:
+    def __init__(self) -> None:
+        self.buffer_size = 1024
+
+    def read_all(self, address: str = "output", format: str = ".txt") -> str:
+        try:
+            with open(address + format, 'r') as file:
+                return file.read()
+        except IOError:
+            return ""
+
+    def write(self, address: str = "output", format: str = ".txt", string: str = "") -> bool:
+        try:
+            with open(address + format, "w") as file:
+                file.write(string)
+            return True
+        except IOError:
+            return False
