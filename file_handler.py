e9d65fc8bb1d3bcab60a69888208f7ae9d085822
masihbr <masihbr@gmail.com>
2022-04-11 12:12:14 +0430

    make file handler static utils functions

--- file_handler.py
+++ file_handler.py
@@ -1,25 +1,21 @@
-class FileHandler:
-    def __init__(self) -> None:
-        self.buffer_size = 32
+def read_chunks(filename: str = "input", format: str = ".txt", buffer_size: int = 4096):
+    with open(filename + format, "r") as file:
+        chunk = file.read(buffer_size)
+        while chunk:
+            yield chunk
+            chunk = file.read(buffer_size)
 
-    def read_chunks(self, filename: str = "input", format: str = ".txt"):
-        with open(filename + format, "rb") as file:
-            chunk = file.read(self.buffer_size)
-            while chunk:
-                yield chunk
-                chunk = file.read(self.buffer_size)
+def read_all(filename: str = "input", format: str = ".txt") -> str:
+    try:
+        with open(filename + format, 'r') as file:
+            return file.read()
+    except IOError:
+        return ""
 
-    def read_all(self, filename: str = "input", format: str = ".txt") -> str:
-        try:
-            with open(filename + format, 'r') as file:
-                return file.read()
-        except IOError:
-            return ""
-
-    def write(self, filename: str = "output", format: str = ".txt", string: str = "") -> bool:
-        try:
-            with open(filename + format, "w") as file:
-                file.write(string)
-            return True
-        except IOError:
-            return False
+def write(filename: str = "output", format: str = ".txt", string: str = "") -> bool:
+    try:
+        with open(filename + format, "w") as file:
+            file.write(string)
+        return True
+    except IOError:
+        return False
