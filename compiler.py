ac3e3955f1b7f61e400ac10e77b17910e4c4bd69
moroukian <moroukianali@gmail.com>
2022-05-04 17:36:40 +0430

    Add Parser class

--- compiler.py
+++ compiler.py
@@ -3,11 +3,7 @@ Masih Beigi - 98101244
 Ali Moroukian - 98106094
 """
 
-from scanner import Scanner
+from parser import Parser
 
 if __name__ == '__main__':
-    scanner = Scanner()
-    while True:
-        token = scanner.get_next_token()
-        if token is None:
-            break
+    Parser().parse()
